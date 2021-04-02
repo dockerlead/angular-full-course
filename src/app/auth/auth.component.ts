c39e6b4e170057f405062d26a78597bf22fc248e
dockerlead <dockerlead@gmail.com>
2021-04-02 16:09:04 -0500

    Add login request and centralize error handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import {Component} from "@angular/core";
 import {NgForm} from "@angular/forms";
+import {Observable} from "rxjs";
 
-import {AuthService} from "./auth.service";
+import {AuthService, AuthResponseData} from "./auth.service";
 
 @Component({
   selector: 'app-auth',
@@ -25,21 +26,27 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    let authObservable: Observable<AuthResponseData>;
+
     this.isLoading = true;
+    
     if (this.isLoginMode) {
-      // ...
+      authObservable = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(
-        resData => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        errorMsg => {
-          this.error = errorMsg;
-          this.isLoading = false;
-        }
-      );
+      authObservable = this.authService.signup(email, password);
     }
+
+    authObservable.subscribe(
+      resData => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      errorMsg => {
+        this.error = errorMsg;
+        this.isLoading = false;
+      }
+    );
+
     form.reset();
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,19 @@
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {catchError} from "rxjs/operators";
 import {throwError} from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
   kind?: string;
+  registered?: boolean;
 }
+
+const FIREBASE_WEB_API_KEY = 'AIzaSyD36JnxLvpbrZuPFrKpVbXL26gVruKzRjQ';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -18,24 +21,42 @@ export class AuthService {
 
   signup(email: string, password: string) {
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD36JnxLvpbrZuPFrKpVbXL26gVruKzRjQ',
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_WEB_API_KEY}`,
       {
         email,
         password,
         returnSecureToken: true
       }
-    ).pipe(
-    catchError(errorRes => {
-      let errorMsg = 'An unknown error occurred!';
-      if (!errorRes.error || !errorRes.error.error) {
-        return throwError(errorMsg);
+    ).pipe(catchError(this.handleError));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_WEB_API_KEY}`,
+      {
+        email,
+        password,
+        returnSecureToken: true
       }
-      switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMsg = 'This email exists already!';
-      }
+    ).pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMsg = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
       return throwError(errorMsg);
-    })
-    );
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMsg = 'This email exists already!';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMsg = 'This email does not exist.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMsg = 'This password is not correct.';
+        break;
+    }
+    return throwError(errorMsg);
   }
 }
