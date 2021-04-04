1c31fcbe7d9654b60f3cb4de3958a3ac78cb6b28
dockerlead <dockerlead@gmail.com>
2021-04-03 23:31:16 -0500

    Separate shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
-import { AlertComponent } from './shared/alert/alert.component';
-
-import { DropdownDirective } from './shared/dropdown.directive';
-import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe.service';
@@ -21,16 +14,13 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
     AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective,
   ],
   imports: [
     BrowserModule,
@@ -40,6 +30,7 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
+    SharedModule,
   ],
   providers: [
     ShoppingListService,
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,6 +1,7 @@
 import {CommonModule} from "@angular/common";
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
+import {SharedModule} from "../shared/shared.module";
 import {ShoppingEditComponent} from "./shopping-edit/shopping-edit.component";
 import {ShoppingListComponent} from "./shopping-list.component";
 import {ShoppingListRoutingModule} from "./shopping-list.routing";
@@ -12,7 +13,7 @@ import {ShoppingListRoutingModule} from "./shopping-list.routing";
   ],
   imports: [
     FormsModule,
-    CommonModule,
+    SharedModule,
     ShoppingListRoutingModule,
   ]
 })
