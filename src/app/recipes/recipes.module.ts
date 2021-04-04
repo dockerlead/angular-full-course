a1d41e7d603456072bb9f00c36f2141dcecb05d4
dockerlead <dockerlead@gmail.com>
2021-04-03 22:49:22 -0500

    Outsource recipes routing to its own module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -7,6 +7,7 @@ import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
 import {RecipeItemComponent} from "./recipe-list/recipe-item/recipe-item.component";
 import {RecipeListComponent} from "./recipe-list/recipe-list.component";
 import {RecipeStartComponent} from "./recipe-start/recipe-start.component";
+import {RecipesRoutingModule} from "./recipes-routing.module";
 import {RecipesComponent} from "./recipes.component";
 
 @NgModule({
@@ -20,16 +21,9 @@ import {RecipesComponent} from "./recipes.component";
   ],
   imports: [
     CommonModule,
-    RouterModule,
     ReactiveFormsModule,
-  ],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent,
+    RouterModule,
+    RecipesRoutingModule,
   ]
 })
 export class RecipesModule {}
