16a9b16053aeb375e1a1ef975dcc26c6e4238712
dockerlead <dockerlead@gmail.com>
2021-04-10 17:07:26 -0500

    Cleanup works

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
-import { RecipeService } from './recipe.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
 })
-export class RecipesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {}
+export class RecipesComponent {
 }
